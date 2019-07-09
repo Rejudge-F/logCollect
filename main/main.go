a74b43b6f9445004c8650ac2eb7d22a9201522c7
zhangfeng <zhangfeng@FengDeMBA.mshome.net>
2019-07-09 20:57:49 +0800

    修改文件目录结构，添加vendor依赖

---
+++ main/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/logs"
+	"kafka-logMgr/etcd"
+	"kafka-logMgr/kafka"
+	"kafka-logMgr/models"
+	"kafka-logMgr/tailf"
+)
+
+var (
+	appConf models.Config
+)
+
+func main() {
+	filename := "../conf/my.conf"
+	adapterType := "ini"
+	err := appConf.LoadConfig(adapterType, filename)
+	if err != nil {
+		fmt.Println("LoadConfig failed", err)
+		return
+	}
+	err = appConf.InitLogger()
+	if err != nil {
+		fmt.Println("InitLogger failed")
+		return
+	}
+
+	err = tailf.InitTailf(appConf.LogCollect, appConf.ChanSize)
+	if err != nil {
+		logs.Error("InitTailf failed", err)
+		return
+	}
+
+	err = kafka.InitKafka(appConf.KafkaIp)
+	if err != nil {
+		logs.Error("InitKafka failed")
+		return
+	}
+
+	err = etcd.InitEtcd(appConf.Etcd.Addr, appConf.Etcd.Key)
+	if err != nil {
+		logs.Error("Init Etcd Failed")
+		return
+	}
+
+	logs.Debug("init all success\n")
+
+	err = ServerRun()
+	if err != nil {
+		logs.Error("Server start failed")
+		return
+	}
+
+	logs.Info("programa running success")
+}
